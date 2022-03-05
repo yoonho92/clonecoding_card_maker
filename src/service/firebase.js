0dfc68c8a2b6a60c095f402ec3af111488b108ec
yoonho <yoonho2015@gmail.com>
2022-03-05 23:05:40 +0900

    프로젝트와 firebase를 셋업하다.

---
+++ src/service/firebase.js
@@ -0,0 +1,12 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  projectId: process.env.REACT_APP_FIREBASE_FIREBASE_DB_URL,
+  databaseURL: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
