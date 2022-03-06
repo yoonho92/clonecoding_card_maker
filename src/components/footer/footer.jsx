819ee136aab1fa854cd3342e4d47a32d9b1a7d31
yoonho <yoonho2015@gmail.com>
2022-03-06 15:48:45 +0900

    로그인 컴포넌트 스타일 설정하다.

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,5 +1,9 @@
 import React from "react";
-
-const Footer = (props) => <h1>Footer</h1>;
+import styles from "./footer.module.css";
+const Footer = (props) => (
+  <footer className={styles.footer}>
+    <p className={styles.title}>Code your dream</p>
+  </footer>
+);
 
 export default Footer;
